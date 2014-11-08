df6d549f59dba5b8deaedd333a99d98c321c23bd
Torbjørn Ludvigsen <tobben@fastmail.fm>
2014-11-08 16:23:54 +0100

    Added template files and init-script v0.0

---
+++ init-report.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Hard coded path...
+# cp ~/scripts/init-rapport/template.d/* .
+git init
+git add .
+# Would like to say here: "Template files added"
+git commit -m "This is init-report.sh."
---
+++ template.d/examplefig.gp
@@ -0,0 +1,4 @@
+set xrange [0:4*pi]
+set xlabel '$x$'
+set ylabel '$\sin{x}$'
+plot sin(x) w l title '$\sin{x}$'
